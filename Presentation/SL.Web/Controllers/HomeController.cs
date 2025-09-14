76b3d8eeea131e7d3c8ce1d93118925f6ae710dd
Onur Karabeyoğlu <123563374+karabeyogluonur@users.noreply.github.com>
2025-09-14 21:57:35 +0300

    Proje katmanları oluşturuldu.

---
+++ Presentation/SL.Web/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SL.Web.Controllers;
+
+public class HomeController : Controller
+{
+    public HomeController()
+    {
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
+
