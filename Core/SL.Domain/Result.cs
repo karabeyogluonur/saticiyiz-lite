f821b12d014fedf5562fb97801e02f220772e8cb
Onur Karabeyoğlu <123563374+karabeyogluonur@users.noreply.github.com>
2025-09-18 11:50:38 +0300

    Result sınıfı eklendi.

---
+++ Core/SL.Domain/Result.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace SL.Domain
+{
+    public record Result
+    {
+        public bool IsSuccess { get; }
+        public string ErrorMessage { get; }
+
+        public static Result Success() => new(true, null);
+        public static Result Failure(string errorMessage) => new(false, errorMessage);
+
+        protected Result(bool isSuccess, string errorMessage)
+        {
+            IsSuccess = isSuccess;
+            ErrorMessage = errorMessage;
+        }
+    }
+
+    public record Result<T> : Result
+    {
+        public T Value { get; }
+
+        public static Result<T> Success(T value) => new(true, value, null);
+        public static Result<T> Failure(string errorMessage) => new(false, default, errorMessage);
+
+        private Result(bool isSuccess, T value, string errorMessage) : base(isSuccess, errorMessage)
+        {
+            Value = value;
+        }
+    }
+}
+
