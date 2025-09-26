ddbe9e47ae9527099d3b2919dfbed00481c12e0e
Onur Karabeyoğlu <123563374+karabeyogluonur@users.noreply.github.com>
2025-09-26 23:56:58 +0300

    Master veritabanı için context factory eklendi.

---
+++ Infrastructre/SL.Persistence/Utilities/MasterDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+using SL.Application.Interfaces.Services;
+using SL.Domain.Entities;
+using SL.Persistence.Contexts;
+using SL.Persistence.Services;
+
+namespace SL.Persistence.Utilities
+{
+    public class MasterDbContextFactory : IDesignTimeDbContextFactory<MasterDbContext>
+    {
+        public MasterDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MasterDbContext>();
+
+            // PostgreSQL için örnek
+            optionsBuilder.UseNpgsql(Configuration.MasterConnectionString);
+
+            return new MasterDbContext(optionsBuilder.Options);
+        }
+    }
+}
+
